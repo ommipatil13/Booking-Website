09450359a71be85a7f794f7ac6d9ce0c69457c0d
Omkar Patil <ommipatil13@gmail.com>
2024-05-06 12:08:38 +0530

    mongoose connect

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,22 @@
-import Express from "express";
+import Express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = Express();
 
-app.use('/', (req, res, next) => {
-    res.send('hello');
-})
+mongoose
+  .connect(
+    `mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.mtqh778.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then(() => app.listen(8080, () => console.log('server and database connected')))
+  .catch((e) => console.log(e));
 
-app.listen(8080, () => {
-    console.log(`server is live ${8080}`)
-})
+// app.use('/', (req, res, next) => {
+//     res.send('hello');
+// })
+
+// app.listen(8080, () => {
+//     console.log(`server is live ${8080}`)
+// })
