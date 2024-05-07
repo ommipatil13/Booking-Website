113030e7a17b42f3ee8083b97172b2f0ed3daff8
Omkar Patil <ommipatil13@gmail.com>
2024-05-07 13:00:41 +0530

    update api

--- backend/controllers/user_controllers.js
+++ backend/controllers/user_controllers.js
@@ -1,4 +1,5 @@
 import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
 
 export const getAllUsers = async (req, res, next) => {
     let users;
@@ -24,9 +25,11 @@ export const signup = async (req, res, next) => {
     if (!name && name.trim() === '' && !email && email.trim() === '' && !password && password.trim() === '') {
         return res.status(422).json({ message: 'Invalid Inputs' });
     }
+
+    const hashedPassword = bcrypt.hashSync(password)
     let user;
     try {
-        user = new User({ name, email, password });
+        user = new User({ name, email, password: hashedPassword });
         user = await user.save();
     } catch (error) {
         // return next(error);
@@ -39,3 +42,28 @@ export const signup = async (req, res, next) => {
 
     return res.status(201).json({ user })
 };
+
+export const updateUser = async (req, res, next) => {
+    const id = req.params.id;
+
+    const { name, email, password } = req.body;
+
+    if (!name && name.trim() === '' && !email && email.trim() === '' && !password && password.trim() === '') {
+        return res.status(422).json({ message: 'Invalid Inputs' });
+    }
+
+    const hashedPassword = bcrypt.hashSync(password)
+    let user;
+    try {
+        user = await User.findByIdAndUpdate(id, { name, email, password: hashedPassword });
+
+    } catch (error) {
+        return console.log(error)
+    }
+
+    if (!user) {
+        return res.status(500).json({ message: "something went wrong" })
+    }
+    res.status(200).json({ message: "Updated Successfully", user });
+
+}
--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -1,9 +1,10 @@
 import Express from 'express';
-import { getAllUsers, signup } from '../controllers/user_controllers.js';
+import { getAllUsers, signup, updateUser } from '../controllers/user_controllers.js';
 
 const router = Express.Router();
 
 router.get('/', getAllUsers);
 router.post('/signup', signup);
+router.put('/:id', updateUser)
 
 export default router;
