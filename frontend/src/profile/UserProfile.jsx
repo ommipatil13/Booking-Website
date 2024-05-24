d56304b883f07c728ba0a610c45d08f6ce5bd8c0
Omkar Patil <ommipatil13@gmail.com>
2024-05-24 12:22:24 +0530

    update user

--- frontend/src/profile/UserProfile.jsx
+++ frontend/src/profile/UserProfile.jsx
@@ -2,7 +2,7 @@ import React, { Fragment, useEffect, useState } from 'react'
 import { deleteBooking, getUserBooking, getUserDetails } from '../api_helpers/api_helpers'
 import { Box } from '@mui/system';
 import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
-import { IconButton, List, ListItem, ListItemText, Typography } from '@mui/material';
+import { Button, IconButton, List, ListItem, ListItemText, TextField, Typography } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 const UserProfile = () => {
@@ -16,12 +16,18 @@ const UserProfile = () => {
         getUserDetails().then((res) => setUser(res.user)).catch((error) => console.log(error))
     }, [])
 
-    // console.log(bookings)
+    // console.log("bookings", bookings)
+    // console.log(user._id)
 
     const handleDelete = (id) => {
 
         deleteBooking(id).then((res) => console.log(res)).catch((error) => console.log(error));
         window.location.reload();
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert('check')
     }
 
     return (
@@ -30,15 +36,34 @@ const UserProfile = () => {
 
             <Fragment>
 
-                {user && < Box width={'30%'} padding={3} flexDirection={'column'} justifyContent={'center'} alignItems={'center'} >
-                    <ManageAccountsIcon sx={{ fontSize: '10rem', ml: 15 }} />
-                    <Typography padding={1} width={'auto'} textAlign={'center'} border={'1px solid #ccc'} borderRadius={6} >
+
+                {user &&
+                    < Box width={'30%'} padding={3} flexDirection={'column'} justifyContent={'center'} alignItems={'center'} >
+                        <ManageAccountsIcon sx={{ fontSize: '10rem', ml: 15 }} />
+
+                        {/* <Typography padding={1} width={'auto'} textAlign={'center'} border={'1px solid #ccc'} borderRadius={6} >
                         Name: {user.name}
-                    </Typography>
-                    <Typography marginTop={1} padding={1} width={'auto'} textAlign={'center'} border={'1px solid #ccc'} borderRadius={6} >
+                        </Typography>
+                        <Typography marginTop={1} padding={1} width={'auto'} textAlign={'center'} border={'1px solid #ccc'} borderRadius={6} >
                         Email: {user.email}
-                    </Typography>
-                </Box>}
+                    </Typography> */}
+
+                        <form onSubmit={handleSubmit} >
+                            <TextField sx={{ border: '1px solid #ccc', width: '100%', borderRadius: 6, Input: { color: 'white', textAlign: "center" } }}
+                                placeholder='Name' variant='standard' type='text' name='name'
+                                value={user.name}
+                                InputProps={{ disableUnderline: true }} />
+
+                            <TextField sx={{ border: '1px solid #ccc', mt: 2, width: '100%', borderRadius: 6, Input: { color: 'white', textAlign: "center" } }}
+                                placeholder='Email' variant='standard' type='text' name='email'
+                                value={user.email}
+                                InputProps={{ disableUnderline: true }} />
+
+                            <Button type='submit' variant='contained' sx={{ marginTop: 3, borderRadius: 6, width: '100%', backgroundColor: '#900C3F' }}  >Update </Button>
+                        </form>
+
+                    </Box>
+                }
 
                 {bookings && bookings.length > 0 && < Box width={'70%'} display={'flex'} flexDirection={'column'} >
 
@@ -69,6 +94,8 @@ const UserProfile = () => {
                 </Box>}
 
             </Fragment >
+
+
 
         </Box >
     )
